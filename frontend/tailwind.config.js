0cd79e697f610322b9b93931131f55888cb7b010
Antonio Ballesteros Alonso <supertoniia@gmail.com>
2025-07-19 17:06:19 +0200

    Modificaciones algunos archivos frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,3 @@
-// archivo: tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,23 +6,7 @@ module.exports = {
     './auth/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'anclora-blue': {
-          light: '#dbeafe',
-          primary: '#3b82f6',
-          secondary: '#2563eb',
-          dark: '#1e40af',
-        },
-        'anclora-slate': {
-          light: '#f1f5f9',
-          DEFAULT: '#64748b',
-          dark: '#1e293b',
-          darker: '#0f172a',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
-
