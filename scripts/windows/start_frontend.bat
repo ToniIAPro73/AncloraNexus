e419c943e9b01bec53ab8355144f9cc8cb259091
Antonio Ballesteros Alonso <supertoniia@gmail.com>
2025-07-23 11:44:45 +0200

    Update start_frontend.bat

--- scripts/windows/start_frontend.bat
+++ scripts/windows/start_frontend.bat
@@ -2,5 +2,5 @@
 ECHO Iniciando servidor Frontend de Anclora Metaform...
 ECHO (Se abrira una nueva ventana. Para detener el servidor, cierrala o presiona Ctrl+C)
 
-REM Inicia una nueva ventana de terminal, navega a la carpeta del frontend y ejecuta el servidor de Next.js
+REM Inicia una nueva ventana de terminal, navega a la carpeta del frontend y ejecuta el servidor de desarrollo
 start "Anclora Frontend" cmd /k "cd frontend && npm run dev"
