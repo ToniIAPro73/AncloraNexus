4ef15170c3c5580b4e9174551115d5c6e2634ace
Antonio Ballesteros Alonso <supertoniia@gmail.com>
2025-07-23 11:27:42 +0200

    Update start_all.bat

--- scripts/windows/start_all.bat
+++ scripts/windows/start_all.bat
@@ -1,9 +1,9 @@
 @echo off
 ECHO Iniciando todos los servicios de Anclora Metaform...
 
-REM Llama a los otros dos scripts para iniciar cada servicio en su propia ventana
-call start_backend.bat
-call start_frontend.bat
+REM Llama a los otros dos scripts usando la ruta del script actual
+call "%~dp0start_backend.bat"
+call "%~dp0start_frontend.bat"
 
 ECHO.
 ECHO Los servidores del Backend y Frontend se estan iniciando en sus propias ventanas.
