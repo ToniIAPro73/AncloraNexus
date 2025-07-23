faacdff57fdf7a50f973333a22ed916e6a775809
Antonio Ballesteros Alonso <supertoniia@gmail.com>
2025-07-23 14:56:27 +0200

    cambios en arranque servicio backend

--- scripts/windows/start_backend.bat
+++ scripts/windows/start_backend.bat
@@ -3,4 +3,4 @@ ECHO Iniciando servidor Backend de Anclora Metaform...
 ECHO (Se abrira una nueva ventana. Para detener el servidor, cierrala o presiona Ctrl+C)
 
 REM Inicia la app como un módulo desde la raíz del proyecto
-start "Anclora Backend" cmd /k "call backend\venv\Scripts\activate.bat && python -m backend.src.main"
+start "Anclora Backend" cmd /k "cd backend && call venv\Scripts\activate.bat && python -m src.main"
