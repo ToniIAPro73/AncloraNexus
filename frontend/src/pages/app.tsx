4343520338e9cf6ffc09c152fe9f2e17092a9457
ToniIAPro73 <supertoniia@gmail.com>
2025-08-29 09:37:57 +0200

    cambios app.tsx

--- frontend/src/pages/app.tsx
+++ frontend/src/pages/app.tsx
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-import { MainLayout } from "../components/Layout/MainLayout";
-import { NewConversorInteligente } from "../components/NewConversorInteligente";
-import { useState } from "react";
-
-export default function AppPage() {
-  const [activeTab, setActiveTab] = useState("Conversor");
-
-  return (
-    <MainLayout activeTab={activeTab} setActiveTab={setActiveTab}>
-      <NewConversorInteligente />
-    </MainLayout>
-  );
-=======
 import NewApp from "../components/NewApp";
 
 export default function AppPage() {
   return <NewApp />;
->>>>>>> 43582ad5a028c4800272a24231e28d97db6bab87
 }
